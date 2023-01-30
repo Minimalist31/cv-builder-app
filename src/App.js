7955f9256a55cb10b3818507c1f53be061efeeb1
Minimalist31 <saumyasarangi3116@gmail.com>
2023-01-31 00:59:27 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Work from "./pages/Work";
-import Writing from "./pages/Writing";
 import Footer from "./components/Footer";
 function App() {
   return (
@@ -14,8 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/writing" element={<Writing />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/pages/Work.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-import React from "react";
-
-const Work = () => {
-  return <div></div>;
-};
-
-export default Work;
